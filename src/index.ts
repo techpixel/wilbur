bfc5be4a43151c461784d774b6abc7eabcc360b6
techpixel <68567672+techpixel@users.noreply.github.com>
2025-10-27 11:30:15 -0400

    forgor to import journal

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "./fanpage/fanpage";
 import "./fanpage/integrity";
 import "./fanpage/hackatime";
 import "./fanpage/git";
+import "./fanpage/journal";
 import "./fanpage/submit";
 
 await app.start(process.env.PORT || 3000).then(() => {
